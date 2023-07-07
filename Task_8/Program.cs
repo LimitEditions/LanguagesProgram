bb1de1203525493213b9486b92ff69b47de9e70f
LimitEditions <138056139+LimitEditions@users.noreply.github.com>
2023-07-07 18:20:56 +0300

    Update Program.cs
    Исправили задачу 8

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -2,6 +2,11 @@
 int number = Convert.ToInt32(Console.ReadLine());
 int i = 1;
 
+if (number < 0) 
+{
+    Console.WriteLine ("Введите положительное число");
+}
+
 while (i <= number)
 {
     if (i%2 == 0)
